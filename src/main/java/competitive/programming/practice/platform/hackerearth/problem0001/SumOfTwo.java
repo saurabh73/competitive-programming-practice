7b9df2c1d57ab47cbf59e499b8b46f451624a6fa
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2021-01-22 00:38:51 +0000

    Add Sample Problems for Leetcode, Hackerearth, Hackerrannk and Codechef

---
+++ src/main/java/competitive/programming/practice/platform/hackerearth/problem0001/SumOfTwo.java
@@ -0,0 +1,24 @@
+package competitive.programming.practice.platform.hackerearth.problem0001;
+
+import competitive.programming.practice.base.ISolution;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+*
+* @author Saurabh Dutta<saurabh73> 
+*
+* @see <a href="https://www.hackerearth.com/practice-onboarding/sum-of-two-1/">https://www.hackerearth.com/practice-onboarding/sum-of-two-1/</a> 
+*
+**/
+public class SumOfTwo implements ISolution {
+    @Override
+    public void solve(InputStream in) throws Exception {
+        Scanner s = new Scanner(in);
+        int num1 = s.nextInt(); // Get first integer
+        int num2 = s.nextInt(); // Get second integer
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
---
+++ src/test/java/competitive/programming/practice/platform/codechef/problem0001/LifeTheUniverseAndEverythingTest.java
@@ -0,0 +1,56 @@
+package competitive.programming.practice.platform.codechef.problem0001;
+import competitive.programming.practice.base.ISolution;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+/**
+*
+* Test for LifeTheUniverseAndEverything.java 
+*
+* @author Saurabh Dutta<saurabh73> 
+*
+* @see <a href="https://www.codechef.com/problems/TEST">https://www.codechef.com/problems/TEST</a> 
+*
+**/
+public class LifeTheUniverseAndEverythingTest {
+
+    private ByteArrayOutputStream buffer;
+
+    @BeforeEach
+    public void setup() {
+        buffer = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(buffer));
+    }
+
+        
+
+    @Test
+    public void case1Test() throws Exception {
+        // Input
+        InputStream inputStream = this.getClass().getResourceAsStream("/codechef/problem0001/input1.txt");
+        // Output
+        InputStream outPutStream = this.getClass().getResourceAsStream("/codechef/problem0001/output1.txt");
+        // Call Method Under Test
+        ISolution problem = new LifeTheUniverseAndEverything();
+        problem.solve(inputStream);
+        //Assertion
+        String actual = buffer.toString().trim();
+        String expected =  IOUtils.toString(outPutStream, Charset.defaultCharset()).trim();
+        assertEquals(expected, actual);
+    }
+    
+
+    @AfterEach
+    public void cleanup() {
+        buffer.reset();
+    }
+}
